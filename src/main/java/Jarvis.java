9b09a00b059bce90df7930cd0b84adbaebce9aee
Basil Kuok <BasilKuokZiXi@gmail.com>
2026-01-22 02:15:32 +0800

    Implement Level 1
    A personalized Jarvis that echo what user type.

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class Jarvis {
     public static void main(String[] args) {
         String horizontalLine = "____________________________________________________________";
         System.out.println(horizontalLine);
         System.out.println(" Hello! I'm Jarvis");
+        System.out.println(" I am your super-intelligent friend.");
         System.out.println(" What can I do for you?");
+        System.out.println(" Tell me what's in your mind and I will echo back to you.");
         System.out.println(horizontalLine);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+
+        Scanner input = new Scanner(System.in);
+        while (input.hasNextLine()) {
+            String echo = input.nextLine();
+            if (echo.trim().equalsIgnoreCase("bye")) {
+                System.out.println(horizontalLine);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(horizontalLine);
+                return;
+            }
+
+            System.out.println(horizontalLine);
+            System.out.println(" " + echo);
+            System.out.println(horizontalLine);
+        }
+
+
+        
     }
 }
